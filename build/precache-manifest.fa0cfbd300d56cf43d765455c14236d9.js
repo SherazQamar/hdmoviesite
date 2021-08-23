2e4f035b38ac0cc1e26b3adfd342e459b71da5af
SherazQamar <qsheraz390@gmail.com>
2021-08-23 19:26:47 +0500

    push

---
+++ build/precache-manifest.fa0cfbd300d56cf43d765455c14236d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c12818843500a27cab9e4d1362f6b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9189f3ea9201f3c50a76",
+    "url": "/static/css/main.1c1ef31e.chunk.css"
+  },
+  {
+    "revision": "7cb1930cabf80796e92f",
+    "url": "/static/js/2.9a2c996c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9a2c996c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9189f3ea9201f3c50a76",
+    "url": "/static/js/main.2756df8e.chunk.js"
+  },
+  {
+    "revision": "6b71224ab26933836c3b",
+    "url": "/static/js/runtime-main.9009ef15.js"
+  }
+]);
